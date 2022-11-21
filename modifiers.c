d9f1ccb3cc7068b1b19db9277d5594ade8f67f7b
Kaydey-Myrtle <113691416+Kaydey-Myrtle@users.noreply.github.com>
2022-11-21 11:52:59 +0100

    Update modifiers.c

--- modifiers.c
+++ modifiers.c
@@ -3,7 +3,7 @@
  * Auth: Ibrahim Ibrahim And Adebayo Isreal
  */
 
-#include "holberton.h"
+#include "main.h"
 
 unsigned int print_width(buffer_t *output, unsigned int printed,
 		unsigned char flags, int wid);
